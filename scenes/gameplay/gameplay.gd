05e40308b1d708827b9c5775ce7a6504f1bfbe79
Davide Cristini <davcri91@gmail.com>
2020-10-01 22:52:15 +0200

    Add level1 base and level placeholder

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
 
-func pre_start(params):
+onready var level_placeholder = $LevelPlaceholder
+
+
+func _ready() -> void:
 	pass
+	
+
+func pre_start(params):
+	if not params.has("level_idx"):
+		load_level(1)
 
 
 func start():
 	pass
+
+
+func load_level(level_idx):
+	# TODO: update to honor level_idx
+	level_placeholder.replace_by_instance()
