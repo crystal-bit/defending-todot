975b91ee3b3a38df4521aff06c0e734cbd3a169a
Davide Cristini <davcri91@gmail.com>
2020-10-05 23:52:35 +0200

    Mega top secret fix during live stream

--- scenes/ui/tower-description.gd
+++ scenes/ui/tower-description.gd
@@ -12,14 +12,14 @@ onready var attack_speed_label : Label = $MarginContainer/VBoxContainer/HBoxCont
 
 
 func _ready() -> void:
-	yield(get_tree(),"idle_frame") #workaround for play scene
+	yield(get_tree(),"idle_frame") # workaround for play scene
 	tower_name_label.text = tower_name
 	tower_description_label.text = tower_description
 	attack_power_label.text = attack_power
 	attack_speed_label.text = attack_speed
-	
-	
+
+
 func show_at_position(position : Vector2):
-	popup()
 	set_as_minsize()
 	rect_global_position = position
+	popup()
