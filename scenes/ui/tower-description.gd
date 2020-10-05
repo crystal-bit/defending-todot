885cfde638b662eba5106dc39b7e9f7307d307f6
Rubino Andrea <andrea.rubino@drivek.com>
2020-10-05 22:12:22 +0200

    renamed scene to comply to guidelines

--- scenes/ui/tower-description.gd
+++ scenes/ui/tower-description.gd
@@ -11,13 +11,13 @@ onready var attack_power_label : Label = $MarginContainer/VBoxContainer/HBoxCont
 onready var attack_speed_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/AttackSpeedContainer/Value
 
 func _ready() -> void:
-	tower_name_label.text = self.tower_name
-	tower_description_label.text = self.tower_description
-	attack_power_label.text = self.attack_power
-	attack_speed_label.text = self.attack_speed
+	tower_name_label.text = tower_name
+	tower_description_label.text = tower_description
+	attack_power_label.text = attack_power
+	attack_speed_label.text = attack_speed
 	
 	
 func show_at_position(position : Vector2):
-		self.popup()
-		self.set_anchors_and_margins_preset(Control.PRESET_CENTER, Control.PRESET_MODE_MINSIZE)
-		self.rect_global_position = position
+		popup()
+		set_as_minsize()
+		rect_global_position = position
