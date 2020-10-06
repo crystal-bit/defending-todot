6e1981878180cb14cbf843adf803feac600d14a2
Tiziano Zambelli <zstiziano@gmail.com>
2020-10-06 22:52:31 +0200

    Add level node and level_idx

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-
-onready var level_placeholder = $LevelPlaceholder
-
+const levelPath = "res://scenes/gameplay/levels/level%d.tscn"
 
 func _ready() -> void:
 	pass
@@ -11,6 +9,8 @@ func _ready() -> void:
 func pre_start(params):
 	if not params.has("level_idx"):
 		load_level(1)
+	else:
+		load_level(params.level_idx)
 
 
 func start():
@@ -18,5 +18,5 @@ func start():
 
 
 func load_level(level_idx):
-	# TODO: update to honor level_idx
-	level_placeholder.replace_by_instance()
+	var levelLoad = load(levelPath % level_idx).instance()
+	$Level.add_child(levelLoad)
