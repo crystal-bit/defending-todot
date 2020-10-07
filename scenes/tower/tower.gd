ab593ebd9be53beaa4047ff7f9fd5f75a0b23c2f
Karbb <andrea.rubino1990@gmail.com>
2020-10-07 14:57:33 +0200

    refactor tower scene

--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -16,8 +16,37 @@ onready var attack_area_shape = $AttackRange/Area2D/CollisionShape2D
 onready var rally_point = $RallyPoint
 onready var sprite = $Sprite
 
+var alpha_area_show : float = .5
+var alpha_area_hide : float = 0
+
+var alpha_area = alpha_area_hide
+
 	
 func _ready() -> void:
+	assert(level != 0, "level cannot be 0. Use initialise method before adding this scene to the tree.")
+	var tower_resource : Tower_Resource = TowerManager.towers_by_level[level][tower_type]
+	_set_tower_attributes(tower_resource)
+	_set_tower_sprite(tower_resource)
+	_set_attack_radius(tower_resource)
+	
+	
+func _draw() -> void:
+	var blue_color : Color = Color.blue
+	blue_color.a = alpha_area
+	draw_circle(Vector2.ZERO, attack_radius, blue_color)
+	
+	
+func initialise(_type, _level):
+	tower_type = _type
+	self.level = _level
+	
+	
+func set_rally_point(position : Vector2):
+	rally_point.global_position = position
+	
+	
+func upgrade():
+	level = level + 1
 	var tower_resource : Tower_Resource = TowerManager.towers_by_level[level][tower_type]
 	_set_tower_attributes(tower_resource)
 	_set_tower_sprite(tower_resource)
@@ -31,29 +60,21 @@ func _set_tower_attributes(tower_resource : Tower_Resource):
 	fire_rate = tower_resource.fire_rate
 	slow_effect = tower_resource.slow_effect
 	damage_area = tower_resource.damage_area
-	update()
 	
 func _set_tower_sprite(tower_resource : Tower_Resource):
 	sprite.texture = tower_resource.sprite
 	
 
-func _draw() -> void:
-	var blue_color : Color = Color.blue
-	blue_color.a = 0.5
-	draw_circle(Vector2.ZERO, attack_radius, blue_color)
-	
-	
-func _set_attack_radius(attack_radius : int):
-	attack_area_shape.shape.radius = attack_radius
+func _set_attack_radius(tower_resource : Tower_Resource):
+	attack_area_shape.shape.radius = tower_resource.attack_radius
 	attack_radius = attack_radius
 	
-	
-func set_rally_point(position : Vector2):
-	rally_point.global_position = position
-	
-	
-func upgrade():
-	level = level + 1
-	var tower_resource : Tower_Resource = TowerManager.towers_by_level[level][tower_type]
-	_set_tower_attributes(tower_resource)
-	_set_tower_sprite(tower_resource)
+
+func _on_AttackRangeShowArea_mouse_entered() -> void:
+	alpha_area = alpha_area_show
+	update()
+
+
+func _on_AttackRangeShowArea_mouse_exited() -> void:
+	alpha_area = alpha_area_hide
+	update()
