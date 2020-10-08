9f6eb05edb3de3583e3034551abffa84bb174823
Tiziano Zambelli <zstiziano@gmail.com>
2020-10-08 11:39:00 +0200

    Change variable names

--- scenes/gameplay/map/checkpoint.gd
+++ scenes/gameplay/map/checkpoint.gd
@@ -1,31 +1,24 @@
 extends Node2D
 
-export var sceneLoad = 1
-export var checkState = 0
+export var scene_load = 1
+export var check_state = 0
 var isSceneLock = true
 
 
 func _ready():
-	if checkState == 0:
-		$Area2D/icon0.visible = true
-	elif checkState == 1:
-		$Area2D/icon1.visible = true
-	elif checkState == 2:
-		$Area2D/icon2.visible = true
+	if check_state == 0:
+		$Area2D/icon0.texture = load("res://assets/sprites/map/checkLock.png")
+	elif check_state == 1:
+		$Area2D/icon0.texture = load("res://assets/sprites/map/checkToDo.png")
+	elif check_state == 2:
+		$Area2D/icon0.texture = load("res://assets/sprites/map/checkDone.png")
 	
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		if event.pressed and checkState != 0:
-			print(sceneLoad)
+		if event.pressed and check_state != 0:
+			print(scene_load)
 			var params = {
-				"level_idx": sceneLoad,
-				"a_number": 10,
-				"a_string": "Ciao mondo!",
-				"an_array": [1, 2, 3, 4],
-				"a_dict": {
-					"name": "test",
-					"val": 15
-				},
+				"level_idx": scene_load,
 			}
 			Game.change_scene("res://scenes/gameplay/gameplay.tscn", params)
