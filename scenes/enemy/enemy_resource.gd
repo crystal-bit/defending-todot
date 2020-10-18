7e46c75a76d42d11ecab8c1bc673fe9b75e20184
Matteo Ruggiero <kirito.poppox@gmail.com>
2020-10-18 15:08:38 +0200

    Update enemy_resource.gd

--- scenes/enemy/enemy_resource.gd
+++ scenes/enemy/enemy_resource.gd
@@ -13,7 +13,7 @@ func get_name():
 		EnemyTypes.ENEMY_TYPES.COMMANDO:
 			return "Commando"
 		EnemyTypes.ENEMY_TYPES.ARMORED_TRANSPORT:
-			return "Armoted Transport"
+			return "Armored Transport"
 		EnemyTypes.ENEMY_TYPES.TANK:
 			return "Tank"
 		EnemyTypes.ENEMY_TYPES.AIRCRAFT:
