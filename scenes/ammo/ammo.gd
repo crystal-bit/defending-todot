0de0e4047f5ce26d7856c5c1421bebbad2bd6f18
Davide Cristini <davcri91@gmail.com>
2020-10-24 16:59:22 +0200

    Integrated ammo.gd logic with existing tower enemies handling.
    Also fixed a bug with tower fire() triggering while the tower was in PREVIEW state.

--- scenes/ammo/ammo.gd
+++ scenes/ammo/ammo.gd
@@ -2,22 +2,19 @@ extends KinematicBody2D
 
 var velocity = Vector2(0.0, 0.0)
 var acceleration = Vector2(0.0, 0.0)
-var direction
-var hit
+var shot_direction
+
 
 func _ready():
-	hit = false
 	acceleration = Vector2(1.0, 1.0)
 
 
 func _process(delta):
+	if !shot_direction:
+		return
 	velocity += acceleration
-	direction = get_parent().shot_direction
+	var dynamics = move_and_collide(velocity * shot_direction * delta)
 	
-	var dynamics = move_and_collide(velocity * direction * delta)
-	
-	if dynamics :
-		if dynamics.collider.is_in_group("enemies") :
-			hit = true
-			hide()
+	if dynamics:
+		if dynamics.collider.is_in_group("enemies"):
 			queue_free() #remove scene from the tree after the strike
--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -3,10 +3,11 @@ class_name Tower
 
 export(Resource) var tower_resource
 
-onready var attack_area_shape = $AttackRange/Area2D/CollisionShape2D
+onready var attack_range_area: Area2D = $AttackRange/AttackRange
 onready var rally_point = $RallyPoint
 onready var sprite = $Sprite
 onready var fire_timer = $FireTimer
+onready var missile_scene = preload("res://scenes/ammo/missile/missile.tscn")
 
 var alpha_area_show : float = .5
 var alpha_area_hide : float = 0
@@ -14,7 +15,6 @@ var alpha_area = alpha_area_hide
 
 var bodies_in_range = []
 var target
-var shot_direction
 
 enum TOWER_STATES {
 	PREVIEW,
@@ -28,11 +28,9 @@ var state = TOWER_STATES.PREVIEW
 func initialise(_resource: Resource):
 	tower_resource = _resource
 
-#Preload missle scene
-onready var missile_scene = preload("res://scenes/ammo/missile/missile.tscn")
-
 
 func _ready() -> void:
+	attack_range_area.monitoring = false
 	load_resource_data(tower_resource)
 
 
@@ -68,14 +66,21 @@ func _on_Area2D_body_exited(body):
 			change_state(TOWER_STATES.GAMEPLAY)
 			target = null
 
+
 func _on_FireTimer_timeout():
 	if state == TOWER_STATES.ATTACKING:
-		fire()
+		fire(target)
 		fire_timer.start()
 
 
+func get_show_direction():
+	pass
+	
+
 func change_state(new_state: int):
 	assert(new_state in TOWER_STATES.values(), "Tower state not in TOWER_STATES list")
+	if new_state == TOWER_STATES.GAMEPLAY:
+		attack_range_area.monitoring = true
 	state = new_state
 
 
@@ -90,27 +95,13 @@ func upgrade():
 
 func load_resource_data(tower_resource : Tower_Resource):
 	sprite.texture = tower_resource.texture
-	attack_area_shape.shape.radius = tower_resource.attack_radius
+	attack_range_area.find_node("CollisionShape2D").shape.radius = tower_resource.attack_radius
 	fire_timer.wait_time = tower_resource.fire_rate
 
 
-
-func fire():
-	#print("firing")
-	#searching for closest target
-	var enemies = get_tree().get_nodes_in_group("enemies")
-	var nearest
-	var near_pos
-	var min_dist = INF
-	for enemy in enemies :
-		var enemy_pos = enemy.get_global_position()
-		var distance = enemy_pos.distance_squared_to(self.get_global_position())
-		if distance < min_dist :
-			min_dist = distance
-			nearest = enemy #storing closest enemy
-			near_pos = nearest.get_global_position()
-		shot_direction = self.get_global_position().direction_to(near_pos)
+func fire(_target: Enemy):
 	var ammo = missile_scene.instance()
-	ammo.set_position(self.position)
-	ammo.set_rotation(self.rotation) #tower should rotate
+	ammo.shot_direction = (_target.global_position - global_position).normalized()
+	ammo.set_position(position)
+	ammo.set_rotation(rotation) #tower should rotate
 	add_child(ammo)
