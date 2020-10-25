db127d4bcac9df8bb461ed95dd562d8b8b14e963
masmartyg <masmartyg@gmail.com>
2020-10-25 16:34:21 +0100

    Made requested changes to PR #85

--- scenes/enemy-commander/enemy-commander.gd
+++ scenes/enemy-commander/enemy-commander.gd
@@ -1,4 +1,3 @@
-tool
 extends Node2D
 
 export(NodePath) var wave_manager_path
@@ -11,29 +10,27 @@ export(Array) var wave_sequence = [
 	[2, 15],
 	[15, 30],
 ]
-var wave_counter = 0
 
 
 func _ready():
 	wave_manager = get_node(wave_manager_path)
 	wave_manager.connect("last_enemy_in_actual_wave_spawned", self, "_on_last_enemy_in_actual_wave_spawned")
 	call_button = get_node(call_button_path)
-	call_button.connect("enemy_start_timeout", self, "_on_enemy_start_timeout")
+	call_button.connect("next_wave_requested", self, "_on_next_wave_requested")
 	yield(get_tree(), "idle_frame") # for nodes to load
 	wait_for_next_wave()
 
 
 func wait_for_next_wave():
-	if wave_counter < wave_sequence.size() and wave_manager.does_next_wave_exists():
-		yield(get_tree().create_timer(wave_sequence[wave_counter][0]), "timeout")
-		call_button.init_call_wave(wave_sequence[wave_counter][1], wave_manager.get_next_wave_description())
-		wave_counter += 1
+	if wave_manager.wave_counter + 1 < wave_sequence.size() and wave_manager.does_next_wave_exists():
+		yield(get_tree().create_timer(wave_sequence[wave_manager.wave_counter + 1][0]), "timeout")
+		call_button.init_call_wave(wave_sequence[wave_manager.wave_counter + 1][1], wave_manager.get_next_wave_description())
 
 
 func _on_last_enemy_in_actual_wave_spawned():
 	wait_for_next_wave()
 
 
-func _on_enemy_start_timeout(ecb: EnemyCallButton):
+func _on_next_wave_requested(ecb: EnemyCallButton):
 	wave_manager.start_next_wave()
 
