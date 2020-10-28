c48ed90c52b380244b743a4118d7f3aabe5bde36
Davide Cristini <davcri91@gmail.com>
2020-10-28 21:26:31 +0100

    Speed up explosion animation and add "disappear" effect when the missile goes far away.
    Refactored also some part of ammo.gd

--- scenes/ammo/ammo.gd
+++ scenes/ammo/ammo.gd
@@ -1,37 +1,46 @@
 extends KinematicBody2D
 class_name Ammo
 
-var velocity = Vector2(0.0, 0.0)
-var acceleration = Vector2(0.0, 0.0)
-var shot_direction
+onready var life_timer = $LifeTimer
+onready var tween = $Tween
+const MAX_VELOCITY = 600
+
+var velocity = 100 # initial velocity
+var acceleration = 2
+var shot_direction := Vector2()
+var travelled_distance = 0
 
 
 func _ready():
-	acceleration = Vector2(1.0, 1.0)
-	$LifeTimer.start()
+	pass
 
 
 func _process(delta):
 	if !shot_direction:
 		return
-	velocity += acceleration
-	var dynamics = move_and_collide(velocity * shot_direction * delta)
-	
-	if dynamics:
-		if dynamics.collider.is_in_group("enemies"):
-			explosion()
+	velocity = min(velocity + acceleration, MAX_VELOCITY)
+	travelled_distance += velocity * delta
+	if travelled_distance > 750 and !tween.is_active():
+		disappear()
+	var collision_data = move_and_collide(velocity * shot_direction * delta)
+
+	if collision_data and collision_data.collider.is_in_group("enemies"):
+		explosion()
 
 
 func _on_Explosion_animation_finished():
 	queue_free()
 
 
-func _on_LifeTimer_timeout():
-	explosion()
+func disappear():
+	tween.interpolate_property(self, "scale", scale, Vector2(), 1.0, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.start()
+	yield(tween, "tween_completed")
+	queue_free()
 
 
 func explosion():
-	shot_direction = 0
+	set_process(false)
 	$Sprite.visible = false
 	$CollisionShape2D.disabled = true
 	$Explosion.visible = true
