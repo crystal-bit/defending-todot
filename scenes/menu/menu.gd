e0603f0f8b52afa7c31cd9cccde7d95331424db6
Andrea1141 <68998830+Andrea1141@users.noreply.github.com>
2020-10-30 20:27:39 +0100

    Added background music for menu, credits and map

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+var music_playing
 
 func _ready():
 	yield(get_tree().create_timer(0.3), "timeout")
 	animation_player.play("TitleShow")
 	yield(animation_player, "animation_finished")
 	animation_player.play("ButtonsShow")
+	if !MenuBackgroundMusic.playing:
+		MenuBackgroundMusic.play()
